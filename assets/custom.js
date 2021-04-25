b994d723b87632f44283fa32c63c72342c6cd0d2
Tofic <mehrazitawfik@gmail.com>
2021-04-25 01:29:51 -0400

    DebutOnDev theme version 0.0.2 : Upload all Files to GitHub repos

---
+++ assets/custom.js
@@ -0,0 +1,27 @@
+
+
+//START: Feature collection slider added from here---------
+
+//var $jq = jQuery.noConflict();
+
+/*
+$(document).ready(function(){
+
+  $('.collection-slider').each(function(){
+
+    
+    var num_slides = 2;
+    if(window.innerWidth >= 750)
+    {
+      var num_slides = $(this).data('slides');
+    }
+    $(this).slick({slidesToShow:num_slides});
+
+
+  });
+
+});
+
+*/
+//END------------------------------------------------------------
+
